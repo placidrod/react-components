7fdb691ebd5fc08c153ffd34790837bc521aa431
Placid Rodrigues <placidrod@gmail.com>
2017-06-02 11:56:36 -0700

    Complete dynamic grocery list

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-// TODO
+var GroceryList = () => (
+  <ul>
+    <GroceryListItem item='mango' />
+    <GroceryListItem item='apple' />
+  </ul>
+);
+
+var GroceryListItem = (props) => (
+  <li>{props.item}</li>
+);
+
+var Mango = () => (
+  <li>mango</li>
+);
+
+var Apple = () => (
+  <li>apple</li>
+);
+
+ReactDOM.render(<GroceryList />, document.getElementById("app"));
