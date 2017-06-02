41a708f46431abf6a2a9efbb2c19283c3515b817
Placid Rodrigues <placidrod@gmail.com>
2017-06-02 12:06:02 -0700

    Refactor GroceryListItem to be a class component

--- app.js
+++ app.js
@@ -5,9 +5,21 @@ var GroceryList = () => (
   </ul>
 );
 
-var GroceryListItem = (props) => (
-  <li>{props.item}</li>
-);
+// var GroceryListItem = (props) => (
+//   <li>{props.item}</li>
+// );
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <li>{this.props.item}</li>
+    );
+  }
+}
 
 var Mango = () => (
   <li>mango</li>
