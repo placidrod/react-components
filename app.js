ebed1de54ff37a92d601660a0d50ef2e0c4fe456
Placid Rodrigues <placidrod@gmail.com>
2017-06-02 12:21:38 -0700

    Make list item bold on mouse over

--- app.js
+++ app.js
@@ -5,28 +5,45 @@ var GroceryList = () => (
   </ul>
 );
 
-// var GroceryListItem = (props) => (
-//   <li>{props.item}</li>
-// );
-
 class GroceryListItem extends React.Component {
+
   constructor(props) {
     super(props);
+
+    this.state = {
+      bold: false
+    };
+  }
+
+  onListItemMouseOver() {
+    this.setState({
+      bold: !this.state.bold
+    });
   }
 
   render() {
+
+    var style = {
+      fontWeight: this.state.bold ? 'bold' : 'normal'
+    };
     return (
-      <li>{this.props.item}</li>
+      <li style={style} onMouseOver={this.onListItemMouseOver.bind(this)}>{this.props.item}</li>
     );
   }
 }
 
-var Mango = () => (
-  <li>mango</li>
-);
-
-var Apple = () => (
-  <li>apple</li>
-);
 
 ReactDOM.render(<GroceryList />, document.getElementById("app"));
+
+
+// var GroceryListItem = (props) => (
+//   <li>{props.item}</li>
+// );
+
+// var Mango = () => (
+//   <li>primitive mango</li>
+// );
+
+// var Apple = () => (
+//   <li>apple</li>
+// );
